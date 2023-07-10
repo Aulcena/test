80087e7393fcce5bc322890b40062f43edaa7b40
Vincent <vshan0816@gmail.com>
2023-07-10 16:30:32 -0400

    finish reject

--- practice-for-ch-ruby-enumerables-long-practice-main/problems.rb
+++ practice-for-ch-ruby-enumerables-long-practice-main/problems.rb
@@ -1,10 +1,11 @@
+require "byebug"
 class Array
     def my_each(&blk) #this needs to take in the block
         #lets iterate through self, the array
         #for each, element we need to call the block
         #after iteration, return original array
         i = 0
-        
+
         while i < self.length
 
             blk.call(self[i])
@@ -16,49 +17,64 @@ class Array
 
 
     # we dont have access to each element
-    def my_select(&blk)
+    # def my_select(&blk)
 
-        array = []
+    #     array = []
         
-        if self.my_each.with_index do |&blk|
+    #     if self.my_each.with_index do |&blk|
 
-            array << self[i]
-            end
+    #         array << self[i]
+    #         end
+    #     end
+
+    #     return array
+
+    # end
+
+    def my_select(&blk)
+        #why does num correspond to self[i]?
+        arr = []
+        self.my_each.with_index do |num|
+
+            arr << num if blk.call(num)
+
         end
 
-        return array
+        arr
 
     end
 
+    def my_reject(&blk)
 
+        arr = []
+        self.my_each do |num|
+
+            arr << num if !blk.call(num)
+
+        end
+
+        arr
+
+    end
 end
+
 
 # use my each method 
 # takes a block
 # returns a  new array containing that only satisfies the blaock
 
 
+a = [1, 2, 3]
+p a.my_reject { |num| num > 1 } # => [1]
+p a.my_reject { |num| num == 4 } # => [1, 2, 3]
 
 
 
 a = [1, 2, 3]
-a.my_select { |num| num > 1 } # => [2, 3]
-a.my_select { |num| num == 4 } # => []
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
 
 
 p "-------------------------------------------"
 
 
-return_value = [1, 2, 3].my_each do |num|
-    puts num
-   end.my_each do |num|
-    puts num
-   end
-   # => 1
-   #    2
-   #    3
-   #    1
-   #    2
-   #    3
-   
-   p return_value  # => [1, 2, 3]
