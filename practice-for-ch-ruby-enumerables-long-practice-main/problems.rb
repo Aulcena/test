e6945431ffb3e0247106518f805e6ed6555df9a7
Vincent <vshan0816@gmail.com>
2023-07-10 15:15:18 -0400

    finish my_each

--- practice-for-ch-ruby-enumerables-long-practice-main/problems.rb
+++ practice-for-ch-ruby-enumerables-long-practice-main/problems.rb
@@ -1,7 +1,30 @@
 class Array
-    def my_each #this needs to take in the block
+    def my_each(&blk) #this needs to take in the block
         #lets iterate through self, the array
         #for each, element we need to call the block
         #after iteration, return original array
+        i = 0
+        
+        while i < self.length
+
+            blk.call(self[i])
+
+            i += 1
+        end
+        self
     end
 end
+
+return_value = [1, 2, 3].my_each do |num|
+    puts num
+   end.my_each do |num|
+    puts num
+   end
+   # => 1
+   #    2
+   #    3
+   #    1
+   #    2
+   #    3
+   
+   p return_value  # => [1, 2, 3]
