622ce3df4a0b95ccbd31a4042c3289e91db5c686
Vincent <vshan0816@gmail.com>
2023-07-10 14:41:00 -0400

    start my_each enumerable

---
+++ practice-for-ch-ruby-enumerables-long-practice-main/problems.rb
@@ -0,0 +1,7 @@
+class Array
+    def my_each #this needs to take in the block
+        #lets iterate through self, the array
+        #for each, element we need to call the block
+        #after iteration, return original array
+    end
+end
